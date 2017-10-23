ba12a98c86a8bf387c333e6a0820e09b77cbbec8
Pranay Kotian <lohitkotian@yahoo.ca>
2017-10-23 13:18:42 -0400

    Added folder for solutions of Canadian Computing Contest (in java). J1 completed.

---
+++ CanadianComputingContest/2017/2017_J2/src/pkg2017_j2/Main.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg2017_j2;
+
+/**
+ * title: Canadian Computing Contest J2
+ * author: Pranay Kotian
+ */
+
+import java.util.Scanner;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        int num = sc.nextInt();
+        int k = sc.nextInt();
+        //Input from users
+        
+        
+    }
+    
+}
