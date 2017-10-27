6cbef252037949725d217b57e7372f703bbe0822
Pranay Kotian <lohitkotian@yahoo.ca>
2017-10-27 01:18:52 -0400

    Started problems 4 and 5 for the Canadian Computing Contest 2017 year

---
+++ CanadianComputingContest/2017/2017_J4/src/pkg2017_j4/Main.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg2017_j4;
+
+/**
+ * title: Canadian Computing Contest J4: Favorite Times
+ * author: Pranay Kotian
+ */
+
+//http://www.cemc.uwaterloo.ca/contests/computing/2017/stage%201/juniorEF.pdf
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        //Learning how to program a clock
+        
+        /*
+        PSEUDOCODE 
+        when minutes = 10, set minutes = 0, tens(minutes)++
+        when tens == 60 and minutes == 10
+            set minutes = 0, tens = 0, hour++
+        when hour = 10,
+            set hour = 0, tens(hour)++
+        when tens(hour) = 1, hour = 2, tens(minutes) = 5, minutes = 10
+            set tens(hour) = 0, hour = 1, tens(minutes) = 0, minutes = 0
+        
+        This code would work when adding minutes individually
+        
+        PSEUDOCODE for entering D minutes all at once
+        
+        
+        */
+    }
+    
+}
---
+++ CanadianComputingContest/2017/2017_J5/src/pkg2017_j5/Main.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg2017_j5;
+
+/**
+ * title: Canadian Computing Contest J5: Nailed it!
+ * author: Pranay Kotian
+ */
+
+//http://www.cemc.uwaterloo.ca/contests/computing/2017/stage%201/juniorEF.pdf
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        
+        
+    }
+    
+}
