37ea1c335bb55a4d717ae81da5288695e9b3d335
Perpenderp <lohitkotian@yahoo.ca>
2017-10-26 22:24:25 -0400

    J3 started

---
+++ CanadianComputingContest/2017/2017_J3/src/pkg2017_j3/Main.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg2017_j3;
+
+/**
+ * title: Canadian Computing Contest J3
+ * author: Pranay Kotian
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        
+        
+    }
+    
+}
