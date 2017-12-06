3c3ffb1b92a907822f187d71cc6d780434761803
Pranay Kotian <lohitkotian@yahoo.ca>
2017-12-05 23:56:27 -0500

    s2 2017 pseudocode

--- CCC/2017/2017_S2/src/pkg2017_s2/Main.java
+++ CCC/2017/2017_S2/src/pkg2017_s2/Main.java
@@ -13,7 +13,33 @@ package pkg2017_s2;
 public class Main {
 
     public static void main(String[] args) {
-       
+       /*
+        planning
+        what we know
+        * started at low tide, measurements alt low/high/low/high
+        * all high tide higher than all low tide
+        * increased range (highs higher and lows lower)
+        
+        * if dataset is odd then the last value must be lowtide
+        
+        dataset must be sorted from lowest to highest and then split
+        1st half: the highest value is the first value of low tide
+        2nd half: the lowest value is the first value of high tide
+        * if dataset odd, median should be considered low tide value
+        
+        pseudocode
+        * given 100 values, quicksort will be the most efficient
+        
+        store values in an arry
+        use quicksort to sort values in array
+        
+        if (N%2 = 1) 
+            range of 0 - (n/2 + 1) will be low tide
+            range of (n/2 + 2) - (n-1)(last value in array) will be high tide
+        else (dataset is even)
+            
+        
+        */
     }
     
 }
