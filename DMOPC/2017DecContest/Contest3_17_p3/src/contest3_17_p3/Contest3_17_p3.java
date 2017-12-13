4c363549b045737996e279adf8d7a37dd28df88b
Pranay Kotian <lohitkotian@yahoo.ca>
2017-12-13 09:12:15 -0500

    pseudocode for p3

--- DMOPC/2017DecContest/Contest3_17_p3/src/contest3_17_p3/Contest3_17_p3.java
+++ DMOPC/2017DecContest/Contest3_17_p3/src/contest3_17_p3/Contest3_17_p3.java
@@ -17,12 +17,32 @@ public class Contest3_17_p3 {
 //                System.out.println(w[i]+"\n"+w[i+1]);
 //        }
         
+        /*
+        
+        Determine how many numbers to add together
+        Determine where to start adding from
+        Compare sum with proceeding values in array
+        Stop if sum greater than value and go back
+        Stop if sum equals value
+        
+        */
+
         int s = 0;
-        for (int i = 0; i < n-2; i++){
-            for (int j = 0; j < i;j++){
-                s+=w[j];
+        for (int i = 1; i < n-1; i++){
+            //i equals the number of values that will be added
+            
+            //max adding position
+            int max = n-1-i;
+            
+            for (int k = 0; k < max; k++){
+                //loop that goes through all starting positions
+                
+                for (int j = 0; j < i; j++){
+                    s+=w[j];
+                }
             }
             
+               
         }
     }
 }
