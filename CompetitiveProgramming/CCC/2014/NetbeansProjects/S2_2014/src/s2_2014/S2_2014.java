a4aa14ecf698d5a0758e135f09e4a7d730f0014c
Pranay Kotian <lohitkotian@yahoo.ca>
2017-12-21 22:47:13 -0500

    started bufferedreader lesson but realized that I need to broaden my scope and cover other important concepts before tackling this specific topic

---
+++ CompetitiveProgramming/CCC/2014/NetbeansProjects/S2_2014/src/s2_2014/S2_2014.java
@@ -0,0 +1,12 @@
+package s2_2014;
+
+import java.util.*;
+
+public class S2_2014 {
+
+    public static void main(String[] args) {
+        
+        
+        
+    }
+}
---
+++ WeeklyLessons/ProgrammedLessons/BufferedReader/src/bufferedreader/BufferedReader.java
@@ -0,0 +1,17 @@
+package bufferedreader;
+
+public class BufferedReader {
+
+    public static void main(String[] args) {
+        //Java.io.BufferedReader Class in Java
+        /*
+        Reads text from a character-input stream, buffering characters so as to
+        provide for efficient reading of characters, arrays, and lines
+        - buffer size may be specified but default size is usually large enough
+        - Generally, read request made for a Reader cuases a corresponding read
+          request to be made of the underlying character or byte system
+        
+        
+        */
+    }
+}
