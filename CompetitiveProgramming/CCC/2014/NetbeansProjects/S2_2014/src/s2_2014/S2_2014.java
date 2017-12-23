14e4d97e8e2d74b874663011add2c6f00e609e9f
Pranay Kotian <lohitkotian@yahoo.ca>
2017-12-22 23:09:29 -0500

    S3_2014 started

--- CompetitiveProgramming/CCC/2014/NetbeansProjects/S2_2014/src/s2_2014/S2_2014.java
+++ CompetitiveProgramming/CCC/2014/NetbeansProjects/S2_2014/src/s2_2014/S2_2014.java
@@ -5,6 +5,15 @@ import java.util.*;
 public class S2_2014 {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        
+        int n = sc.nextInt(); //number of students in the class
+        
+        //lookup how to input space separated strings in java
+        
+        
+        //two arrays will hold the two sets of people
+        
         
         
         
---
+++ WeeklyLessons/ProgrammedLessons/S3_2014/src/s3_2014/S3_2014.java
@@ -0,0 +1,20 @@
+package s3_2014;
+
+import java.util.*;
+
+public class S3_2014 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        
+        int t = sc.nextInt(); //number of different tests that will be run
+        int n;
+        
+        for (int i = 0; i < t; i++) {
+            n = sc.nextInt();
+            for (int p = 0; p < n; p++) {
+                
+            }
+        }
+    }
+}
